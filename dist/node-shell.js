5bbfbb84961130730dba7d8053bcfec275d4ebd8
fabsharp <Omahabitch14>
2020-02-26 14:54:41 +0100

    0.9.12

--- dist/node-shell.js
+++ dist/node-shell.js
@@ -4,7 +4,7 @@
   (global = global || self, factory(global.shell = {}));
 }(this, (function (exports) { 'use strict';
 
-  console.log("node-shell.js v0.9.11")
+  console.log("node-shell.js v0.9.12")
 
   var _consoleLog = false;
   var settings = {
