f47a835ae0c6ee8b140dcd2ba02414e88270bbe9
devAxeon <dev@axeon.fr>
2020-03-02 16:15:00 +0100

    Remove commented code unused

--- dist/node-shell.js
+++ dist/node-shell.js
@@ -163,31 +163,6 @@
       });
   }
 
-  /*import {ShellEntry} from "../ShellEntry";
-  const http = require('http');
-  const fs = require('fs');
-  const fetch = require('node-fetch');
-  const path = require('path');
-  import {getEntry} from "./utils/getEntry";
-  import * as log from './log';
-  import {mkdir} from "./mkdir";
-
-  export const download = (url : string, dest : string) : Promise<ShellEntry> => {
-      return new Promise((resolve, reject) => {
-          fetch(url).then((res) => {
-              // dest could be deep folder
-              let parent = path.dirname(dest);
-              mkdir(parent).then(() => {
-                  const writeStream = fs.createWriteStream(dest);
-                  res.body.pipe(writeStream);
-                  getEntry(dest).then(entry => {
-                      log.info("file downloaded");
-                      resolve(entry);
-                  }, reject);
-              }, reject);
-          }, reject);
-      });
-  }*/
   var fs$2 = require('fs');
   var path$1 = require('path');
   var download = function (url, dest, progressCallback) {
